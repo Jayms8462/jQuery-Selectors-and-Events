a3892095ad258452bd440c4c896f43a005974e59
James Portune <Jayms.Portune@gmail.com>
2019-07-19 18:24:04 -0700

    Saving Progress

--- js/app.js
+++ js/app.js
@@ -1,28 +1,36 @@
-'use strict';
+const allHorns = [];
+const allKeys = [];
 
-
-
-//create MyHorn object by passing an object
-function MyHorn(horn){
-  this.image_url=horn.image_url;
-  this.title=horn.title;
-  this.description=horn.description;
-  this.keyword=horn.keyword;
-  this.horns=horn.horns;
-  loadOptions(this.keyword);
+const MyHorns = function(image_url, title, description, keyword, horns){
+  this.image_url = image_url;
+  this.title = title;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
+  allHorns.push(this);
+  allKeys.push(this.keyword);
 }
 
-//an array to hold all the MyHorn
-MyHorn.allMyHorn=[];
+MyHorns.readJson = function(){
+  const filePath = 'data/page-1.json';
+  const fileType = 'json';
+  $.get(filePath, fileType).then(myHornsJson => {
+    myHornsJson.forEach(horn => {
+      new MyHorns(horn.image_url, horn.title, horn.description, horn.keyword, horn.keyword)
+    });
+    allHorns.forEach(MyHorn => {
+      let keyword = MyHorn.keyword;
+      MyHorn.render(keyword);
+    });
+  });
+};
 
-MyHorn.prototype.render=function(){
-
+MyHorns.prototype.render = function(keyword) {
+  $('select').append($('<option>', {text:keyword}));
   $('main').append('<div class="clone"></div>');
   let hornClone=$('div[class="clone"]');
   let hornHtml=$('#photo-template').html();
-
   hornClone.html(hornHtml);
-
   hornClone.find('h2').text(this.title);
   hornClone.find('img').attr('src',this.image_url);
   hornClone.find('p').text(this.description);
@@ -30,54 +38,6 @@ MyHorn.prototype.render=function(){
   hornClone.find('p').text(this.MyHorn);
   hornClone.removeClass('clone');
   hornClone.attr('class',this.keyword);
-  
 }
 
-// MyHorn.prototype.tohtml=function(){
-//   let $target=$('#handlebar').html();
-//   let $source=Handlebars.compile($target);
-//   return $source(this);
-// };
-
-MyHorn.readJson=(filename)=>{
-
-  $.get(filename,'json')
-
-    .then(data=>{
-
-      data.forEach(horn=>{
-        MyHorn.allMyHorn.push(new MyHorn(horn));
-      });
-    })
-    // DOM
-    .then(MyHorn.loadMyHorn)
-    .then(MyHorn.imgselect)
-
-};
-
-MyHorn.loadMyHorn=()=>MyHorn.allMyHorn.forEach(horn=> horn.render());
-
-function loadOptions(arr) {
-  // $('option').append('<option>test</option>');
-  console.log(arr);
-}
-
-// MyHorn.loadMyHorn=()=>
-//   MyHorn.allMyHorn.forEach(horn=>{$('#photo-template').append(horn.tohtml());});
-
-
-
-
-// $('#one').on('click',function(){
-//   $('div').remove();
-//   //clear the dropdown list
-//   $('option').remove();
-//   MyHorn.allMyHorn=[];
-
-//   //load the page
-//   $(()=>MyHorn.readJson('data/page-1.json'));
-// });
-
-
-
-$(()=>MyHorn.readJson('data/page-1.json'));
+MyHorns.readJson();
